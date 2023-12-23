ca87e22725990b8e9147b08f56f8df29c2892375
weslemarques <wesleremarques@gmail.com>
2023-12-22 21:01:07 -0300

    feature - validation user register

--- src/main/java/br/com/kyw/project_kyw/application/repositories/UserRepository.java
+++ src/main/java/br/com/kyw/project_kyw/application/repositories/UserRepository.java
@@ -10,4 +10,6 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
+    boolean existsByUsername(String username);
 }
