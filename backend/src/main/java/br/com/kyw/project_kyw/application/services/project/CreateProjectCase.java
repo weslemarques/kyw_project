56cbd839141890ede48cd7ea221efe289e0cd999
weslemarques <wesleremarques@gmail.com>
2024-01-09 20:13:04 -0300

    feature (wesle) - create logic project

--- backend/src/main/java/br/com/kyw/project_kyw/application/services/project/CreateProjectCase.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/project/CreateProjectCase.java
@@ -5,17 +5,22 @@ import br.com.kyw.project_kyw.adapters.dtos.request.ProjectCreateDTO;
 import br.com.kyw.project_kyw.adapters.dtos.response.ProjectResponseDTO;
 import br.com.kyw.project_kyw.application.exceptions.UserNotFoundExeception;
 import br.com.kyw.project_kyw.application.repositories.ProjectRepository;
+import br.com.kyw.project_kyw.application.repositories.ProjectRoleRepository;
 import br.com.kyw.project_kyw.application.repositories.UserRepository;
 import br.com.kyw.project_kyw.application.services.utils.FileStorageService;
 import br.com.kyw.project_kyw.application.services.utils.SendNotification;
 import br.com.kyw.project_kyw.core.entities.Project;
+import br.com.kyw.project_kyw.core.entities.ProjectRole;
 import br.com.kyw.project_kyw.core.entities.User;
+import br.com.kyw.project_kyw.core.enums.Title;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,7 +28,7 @@ import java.util.UUID;
 public class CreateProjectCase {
     private final UserRepository userRepository;
     private final ModelMapper mapper;
-    private final ProjectRepository projectRepository;
+    private final ProjectRoleRepository projectRoleRepository;
     private final SendNotification sendNotification;
     private final FileStorageService fileStorageService;
 
@@ -32,30 +37,28 @@ public class CreateProjectCase {
     String text = "Você foi convidado para entrar  no projeto" + projectName;
 
 
-    public CreateProjectCase(UserRepository userRepository, ProjectRepository projectRepository, ModelMapper mapper, SendNotification sendInvitation, FileStorageService fileStorageService) {
+    public CreateProjectCase(UserRepository userRepository, ProjectRoleRepository projectRoleRepository, ModelMapper mapper, SendNotification sendInvitation, FileStorageService fileStorageService) {
         this.userRepository = userRepository;
-        this.projectRepository = projectRepository;
+        this.projectRoleRepository = projectRoleRepository;
         this.mapper = mapper;
         this.sendNotification = sendInvitation;
         this.fileStorageService = fileStorageService;
     }
     public ProjectResponseDTO createProject(ProjectCreateDTO projectRequest){
+        List<ProjectRole> projectRoles = new ArrayList<>();
         projectName = projectRequest.getName();
-        User userAdmin = userRepository.findById(projectRequest.getUserAdmin()).orElseThrow(() -> new UserNotFoundExeception("User not found in create project"));
+        User userCreator = userRepository.findById(projectRequest.getUserAdmin()).orElseThrow(() -> new UserNotFoundExeception("User not found in create project"));
         Project projectEntity = mapper.map(projectRequest, Project.class);
-        projectEntity.addAdmin(userAdmin);
+        projectEntity.setCreator(userCreator);
+        Path pathUrlImage = fileStorageService.storageFile(projectRequest.getImage());
+        projectEntity.setImageUrl(pathUrlImage.toUri().getPath());
         for (String email: projectRequest.getMembers()) {
-            System.out.println(projectRequest.getMembers());
             Optional<User> user  = userRepository.findByEmail(email);
-            System.out.println(user);
-            user.ifPresent(projectEntity::addMembers);
+            user.ifPresent(value -> projectRoles.add(new ProjectRole(value, projectEntity, Title.MEMBER)));
             user.ifPresent(value -> sendNotification
                     .senderByEmail(new Email(value.getId(), value.getEmail(), subject, text)));
         }
-        Path pathUrlImage = fileStorageService.storageFile(projectRequest.getImage());
-        System.out.println(projectEntity.getMembers());
-        projectEntity.setImageUrl(pathUrlImage.toUri().getPath());
-        projectEntity = projectRepository.save(projectEntity);
+        projectRoleRepository.saveAll(projectRoles);
         return mapper.map(projectEntity, ProjectResponseDTO.class);
     }
 
