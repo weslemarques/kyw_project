e712d6ca03f737b4d68951a57c349d4be69d677f
weslemarques <wesleremarques@gmail.com>
2024-03-09 21:11:12 -0300

    bug - config profile

--- backend/src/main/java/br/com/kyw/project_kyw/ProjectKywApplication.java
+++ backend/src/main/java/br/com/kyw/project_kyw/ProjectKywApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class ProjectKywApplication {
 
     public static void main(String[] args) {
