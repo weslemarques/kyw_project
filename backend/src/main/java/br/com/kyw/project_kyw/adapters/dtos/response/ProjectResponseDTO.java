f5229e6fa1ec7ee9533ae32fa1391e2cfe8ff10f
weslemarques <wesleremarques@gmail.com>
2024-03-19 23:25:03 -0300

    feature -refactoring refactoring project response return

--- backend/src/main/java/br/com/kyw/project_kyw/adapters/dtos/response/ProjectResponseDTO.java
+++ backend/src/main/java/br/com/kyw/project_kyw/adapters/dtos/response/ProjectResponseDTO.java
@@ -1,7 +1,7 @@
 package br.com.kyw.project_kyw.adapters.dtos.response;
 
+import br.com.kyw.project_kyw.core.entities.ProjectRole;
 import br.com.kyw.project_kyw.core.entities.Task;
-import br.com.kyw.project_kyw.core.entities.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,11 +22,9 @@ public class ProjectResponseDTO implements Serializable {
     private UUID id;
     private String name;
     private String description;
-    private List<UserResponseDTO> admins = new ArrayList<>();
     private String imageUrl;
     private String linkGroup;
-    private List<UserResponseDTO> members = new ArrayList<>();
-
+    private List<ProjectRole> projectRoles = new ArrayList<>();
     private List<Task> tasks = new ArrayList<>();
 
 }
