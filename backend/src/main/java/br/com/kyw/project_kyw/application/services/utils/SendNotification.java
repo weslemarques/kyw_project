efe78623e4cc989065b408a720e4423e2fb5c279
weslemarques <wesleremarques@gmail.com>
2024-03-19 23:25:10 -0300

    feature -refactoring refactoring project response return

--- backend/src/main/java/br/com/kyw/project_kyw/application/services/utils/SendNotification.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/utils/SendNotification.java
@@ -2,7 +2,9 @@ package br.com.kyw.project_kyw.application.services.utils;
 
 
 import br.com.kyw.project_kyw.adapters.dtos.emails.Email;
+import org.springframework.scheduling.annotation.Async;
 
 public interface SendNotification {
+    @Async
     void senderByEmail(Email email);
 }
