f9481d86e2ab5d79bde86299542a240b882f44fb
weslemarques <wesleremarques@gmail.com>
2024-03-21 22:39:33 -0300

    refactoring - mapper

--- backend/src/main/java/br/com/kyw/project_kyw/adapters/dtos/request/ProjectUpadateDTO.java
+++ backend/src/main/java/br/com/kyw/project_kyw/adapters/dtos/request/ProjectUpadateDTO.java
@@ -1,5 +1,8 @@
 package br.com.kyw.project_kyw.adapters.dtos.request;
 
+import lombok.Getter;
+
+@Getter
 public class ProjectUpadateDTO {
     private String name;
     private String description;
--- backend/src/main/java/br/com/kyw/project_kyw/application/services/project/ProjectServiceImpl.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/project/ProjectServiceImpl.java
@@ -5,46 +5,48 @@ import br.com.kyw.project_kyw.adapters.dtos.response.ProjectResponseDTO;
 import br.com.kyw.project_kyw.application.exceptions.ResourceNotFound;
 import br.com.kyw.project_kyw.application.exceptions.UserNotFoundExeception;
 import br.com.kyw.project_kyw.application.repositories.ProjectRepository;
-import org.modelmapper.ModelMapper;
+import br.com.kyw.project_kyw.application.services.utils.Mapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 import java.util.UUID;
 
 @Service
 public class ProjectServiceImpl {
 
     private final ProjectRepository projectRepository;
-    private final ModelMapper mapper;
+    private final Mapper mapper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ModelMapper mapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, Mapper mapper) {
         this.projectRepository = projectRepository;
         this.mapper = mapper;
     }
-    public void delete(UUID id){
-        var user  = projectRepository
+
+    public void delete(UUID id) {
+        var user = projectRepository
                 .findById(id).orElseThrow(() -> new UserNotFoundExeception("User not Found"));
         user.setDeleted(true);
         projectRepository.save(user);
     }
 
-    public ProjectResponseDTO update(ProjectUpadateDTO projectUpadateDTO, UUID id){
+    public ProjectResponseDTO update(ProjectUpadateDTO projectUpadateDTO, UUID id) {
         var project = projectRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundExeception("User not Found")); // Para que serve essa verificação?
-        mapper.map(projectUpadateDTO, project);
+        project.setDescription(projectUpadateDTO.getDescription());
+        project.setName(projectUpadateDTO.getName());
         project = projectRepository.save(project);
-       return mapper.map(project, ProjectResponseDTO.class);
+        return mapper.entityForProjectResponse(project);
     }
 
+    public ProjectResponseDTO getById(UUID projectId) {
+        var project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new ResourceNotFound("Project Not Found"));
+        return mapper.entityForProjectResponse(project);
+    }
 
-        public ProjectResponseDTO getById(UUID projectId) {
-            var project =  projectRepository.findById(projectId)
-                    .orElseThrow(()-> new ResourceNotFound("Project Not Found"));
-            return mapper.map(project, ProjectResponseDTO.class);
-        }
-
-        public Page<ProjectResponseDTO> getAll(Pageable pageable) {
-            var listProject =  projectRepository.findAll(pageable);
-            return listProject.map(project -> mapper.map(project, ProjectResponseDTO.class));
-        }
+    public Page<ProjectResponseDTO> getAll(Pageable pageable) {
+        var listProject = projectRepository.findAll(pageable);
+        return listProject.map(mapper::entityForProjectResponse);
+    }
 }
