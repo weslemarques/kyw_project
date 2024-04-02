b368a564667ee4d937396e5481f8e185a146563e
weslemarques <wesleremarques@gmail.com>
2024-04-01 22:04:06 -0300

    fix - fixing bug admin

--- backend/src/main/java/br/com/kyw/project_kyw/adapters/dtos/request/UserRegisterDTO.java
+++ backend/src/main/java/br/com/kyw/project_kyw/adapters/dtos/request/UserRegisterDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Setter @Getter
 @UserRegisterValid
+@NoArgsConstructor
 public class UserRegisterDTO implements Serializable {
 
     @NotBlank
