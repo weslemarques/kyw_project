ae5c878dc418266a50484881365597dc2b2a55a5
weslemarques <wesleremarques@gmail.com>
2024-04-24 23:03:49 -0300

    refactoring - security endpoints

--- backend/src/main/java/br/com/kyw/project_kyw/infra/config/WebSocketConfig.java
+++ backend/src/main/java/br/com/kyw/project_kyw/infra/config/WebSocketConfig.java
@@ -2,6 +2,8 @@ package br.com.kyw.project_kyw.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-@CrossOrigin("*")
+@EnableWebSocketSecurity
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
