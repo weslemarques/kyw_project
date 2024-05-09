44346b25267171ee23ebbfe36c64459b1fbbcdee
weslemarques <wesleremarques@gmail.com>
2024-05-09 19:37:45 -0300

    fix - transforming

--- backend/src/main/java/br/com/kyw/project_kyw/application/services/utils/Mapper.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/utils/Mapper.java
@@ -4,6 +4,7 @@ import br.com.kyw.project_kyw.adapters.dtos.response.*;
 import br.com.kyw.project_kyw.core.entities.Message;
 import br.com.kyw.project_kyw.core.entities.Project;
 import br.com.kyw.project_kyw.core.entities.ProjectRole;
+import br.com.kyw.project_kyw.core.entities.Task;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -21,13 +22,13 @@ public class Mapper {
 
     public ProjectResponseDTO entityForProjectResponse(Project project){
         var projectResponse = mapper.map(project, ProjectResponseDTO.class);
-        projectResponse.setTasks(project.getTasks());
+        projectResponse.setTasks(project.getTasks().stream().map(t -> mapper.map(t,TaskResponse.class)).toList());
         return projectResponse;
     }
 
     public Project dtoForProjectEntity(ProjectResponseDTO dto){
         var project = mapper.map(dto, Project.class);
-        project.setTasks(dto.getTasks());
+        project.setTasks(dto.getTasks().stream().map(t -> mapper.map(t, Task.class)).toList());
         return project;
     }
 
