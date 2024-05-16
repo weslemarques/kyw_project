2e53702d69b9bb5a028fe299e14613e2e7b16243
weslemarques <wesleremarques@gmail.com>
2024-05-15 23:00:38 -0300

    refactoring - level access projects

--- backend/src/main/java/br/com/kyw/project_kyw/application/services/ProjectRoleService.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/ProjectRoleService.java
@@ -1,41 +1,52 @@
 package br.com.kyw.project_kyw.application.services;
 
 import br.com.kyw.project_kyw.adapters.dtos.response.ProjectRoleResponse;
+import br.com.kyw.project_kyw.application.repositories.ProjectRepository;
 import br.com.kyw.project_kyw.application.repositories.ProjectRoleRepository;
-import br.com.kyw.project_kyw.application.repositories.UserRepository;
+import br.com.kyw.project_kyw.core.entities.Project;
 import br.com.kyw.project_kyw.core.entities.ProjectRole;
 import br.com.kyw.project_kyw.infra.security.Auth;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.lang.module.ResolutionException;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
 public class ProjectRoleService {
     private final ProjectRoleRepository projectRoleRepository;
-    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
     private final ModelMapper mapper;
-
-    public ProjectRoleService(ProjectRoleRepository projectRoleRepository, ModelMapper mapper,UserRepository userRepository) {
+    public ProjectRoleService(ProjectRoleRepository projectRoleRepository, ModelMapper mapper, ProjectRepository projectRepository) {
         this.projectRoleRepository = projectRoleRepository;
         this.mapper = mapper;
-        this.userRepository = userRepository;
+        this.projectRepository = projectRepository;
     }
 
-    public Page<ProjectRoleResponse> getProjectRoleByUser(UUID userId, Pageable pageable){ // TODO pegar ID pela autenticação
-        var pageProRole = projectRoleRepository.findProjectRolesByUser_Id(userId, pageable);
+    public Page<ProjectRoleResponse> getProjectRoleByUser(Pageable pageable){
+        var pageProRole = projectRoleRepository.findProjectRolesByUser_Id(Auth.getUserAuthenticate().getId(), pageable);
         return pageProRole.map(pRole -> mapper.map(pRole, ProjectRoleResponse.class));
     }
 
-    public String getProjectRoleByProjectAndUser(UUID projectId){ //TODO fazer utilizando autenticaçõa
+    public String getProjectRoleByProjectAndUser(UUID projectId){
+        if(isCreator(projectId)){
+            return "CREATOR";
+        }
         ProjectRole projectRole = projectRoleRepository.findProjectRoleByProject_IdAndUser_Id(projectId, Auth.getUserAuthenticate().getId())
                 .orElseThrow(() -> new ResolutionException("Usuário não tem papel nesse projeto"));
         return projectRole.getTitle().toString();
     }
     public void changeRoleOfAMember(){
 
+    }
+    public boolean isCreator(UUID projectId){
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new ResolutionException("Usuário não tem papel nesse projeto"));
+        UUID idUserLoged = Auth.getUserAuthenticate().getId();
+        return Objects.equals(idUserLoged.toString(), project.getCreator().getId().toString());
     }
 }
--- backend/src/main/java/br/com/kyw/project_kyw/application/services/project/ProjectServiceImpl.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/project/ProjectServiceImpl.java
@@ -2,55 +2,60 @@ package br.com.kyw.project_kyw.application.services.project;
 
 import br.com.kyw.project_kyw.adapters.dtos.request.ProjectUpadateDTO;
 import br.com.kyw.project_kyw.adapters.dtos.response.ProjectResponseDTO;
+import br.com.kyw.project_kyw.application.acess.AccessProjectLevel;
 import br.com.kyw.project_kyw.application.exceptions.AuthorizationException;
 import br.com.kyw.project_kyw.application.exceptions.ResourceNotFound;
 import br.com.kyw.project_kyw.application.exceptions.UserNotFoundExeception;
 import br.com.kyw.project_kyw.application.repositories.ProjectRepository;
-import br.com.kyw.project_kyw.application.services.ProjectRoleService;
 import br.com.kyw.project_kyw.application.services.utils.Mapper;
-import br.com.kyw.project_kyw.infra.security.Auth;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
 @Service
 public class ProjectServiceImpl {
 
     private final ProjectRepository projectRepository;
     private final Mapper mapper;
-    private final ProjectRoleService projectRoleService;
+    private final AccessProjectLevel accessProjectLevel;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, Mapper mapper, ProjectRoleService projectRoleService) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, Mapper mapper, AccessProjectLevel accessProjectLevel) {
         this.projectRepository = projectRepository;
         this.mapper = mapper;
-        this.projectRoleService = projectRoleService;
+        this.accessProjectLevel = accessProjectLevel;
     }
 
-    public void delete(UUID id) {
-        var user = projectRepository
-                .findById(id).orElseThrow(() -> new UserNotFoundExeception("Usuario não encontrado"));
-        user.setDeleted(true);
-        projectRepository.save(user);
+    public void delete(UUID projectId) {
+        if(accessProjectLevel.isCreator(projectId)){
+            var user = projectRepository
+                    .findById(projectId).orElseThrow(() -> new UserNotFoundExeception("Usuario não encontrado"));
+            user.setDeleted(true);
+            projectRepository.save(user);
+            return;
+        }
+        throw new AuthorizationException("Você não tem autorização para deletar esse projeto");
     }
 
     public ProjectResponseDTO update(ProjectUpadateDTO projectUpadateDTO, UUID projectId) {
-        var role = projectRoleService.getProjectRoleByProjectAndUser(projectId);
-        var project = projectRepository.findById(projectId)
-                .orElseThrow(() -> new UserNotFoundExeception("Usuario não encontrado"));
-        if(role.equalsIgnoreCase("ADMIN") || project.getCreator().getId() == Auth.getUserAuthenticate().getId()){
-        project.setDescription(projectUpadateDTO.getDescription());
-        project.setName(projectUpadateDTO.getName());
-        project = projectRepository.save(project);
-        return mapper.entityForProjectResponse(project);}
+        if(accessProjectLevel.isCreatorOrAdmin(projectId)){
+            var project = projectRepository.findById(projectId)
+                    .orElseThrow(() -> new UserNotFoundExeception("Usuario não encontrado"));
+            project.setDescription(projectUpadateDTO.getDescription());
+            project.setName(projectUpadateDTO.getName());
+            project = projectRepository.save(project);
+            return mapper.entityForProjectResponse(project);
+        }
         throw new AuthorizationException("Você não tem autorização para atualizar esse projeto");
     }
 
     public ProjectResponseDTO getById(UUID projectId) {
-        var project = projectRepository.findById(projectId)
-                .orElseThrow(() -> new ResourceNotFound("Projeto não encontrado"));
-        return mapper.entityForProjectResponse(project);
+        if(accessProjectLevel.canAcessProject(projectId)){
+            var project = projectRepository.findById(projectId)
+                    .orElseThrow(() -> new ResourceNotFound("Projeto não encontrado"));
+            return mapper.entityForProjectResponse(project);
+        }
+        throw new AuthorizationException("Você não tem autorização para acessar esse projeto");
     }
 
     public Page<ProjectResponseDTO> getAll(Pageable pageable) {
