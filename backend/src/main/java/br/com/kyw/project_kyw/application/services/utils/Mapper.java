4a88008d7fa3d9b892296d9e9d5d5eebda96dc16
weslemarques <wesleremarques@gmail.com>
2024-05-19 19:43:05 -0300

    feature - change title user

--- backend/src/main/java/br/com/kyw/project_kyw/application/services/utils/Mapper.java
+++ backend/src/main/java/br/com/kyw/project_kyw/application/services/utils/Mapper.java
@@ -3,10 +3,10 @@ package br.com.kyw.project_kyw.application.services.utils;
 import br.com.kyw.project_kyw.adapters.dtos.ProjectIncludeDTO;
 import br.com.kyw.project_kyw.adapters.dtos.UserIncludeDTO;
 import br.com.kyw.project_kyw.adapters.dtos.request.ProjectCreateDTO;
-import br.com.kyw.project_kyw.adapters.dtos.response.*;
+import br.com.kyw.project_kyw.adapters.dtos.response.MessageResponse;
+import br.com.kyw.project_kyw.adapters.dtos.response.ProjectResponseDTO;
 import br.com.kyw.project_kyw.core.entities.Message;
 import br.com.kyw.project_kyw.core.entities.Project;
-import br.com.kyw.project_kyw.core.entities.Task;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -22,16 +22,13 @@ public class Mapper {
 
     public ProjectResponseDTO entityForProjectResponse(Project project){
         var projectResponse = mapper.map(project, ProjectResponseDTO.class);
-        //projectResponse.setTasks(project.getTasks().stream().map(t -> mapper.map(t,TaskResponse.class)).toList());
         var creator = new UserIncludeDTO(project.getCreator().getId(), project.getCreator().getNickname());
         projectResponse.setCreator(creator);
         return projectResponse;
     }
 
     public Project dtoForProjectEntity(ProjectCreateDTO dto){
-        var project = mapper.map(dto, Project.class);
-        //project.setTasks(dto.getTasks().stream().map(t -> mapper.map(t, Task.class)).toList());
-        return project;
+        return mapper.map(dto, Project.class);
     }
 
     public MessageResponse entityForDTOMessage(Message message){
